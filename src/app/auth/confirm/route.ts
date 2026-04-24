0ce72c06855f44391c6b6da47393531e4ea1723d
Samuel Rebollar <acostasamuel947@gmail.com>
2026-04-23 20:34:03 -0600

    Vercel

--- src/app/auth/confirm/route.ts
+++ src/app/auth/confirm/route.ts
@@ -7,7 +7,7 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
   const token_hash = searchParams.get('token_hash')
   const type = searchParams.get('type') as EmailOtpType | null
-  const next = searchParams.get('next') ?? '/reset-password'
+  let next = searchParams.get('next') ?? '/reset-password'
   const code = searchParams.get('code')
 
   const cookieStore = await cookies()
